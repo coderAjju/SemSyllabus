dbfd13bf5888c2829daca56d5cd61d52f473efbd
coderAjju <upadhyayajay437@gmail.com>
2024-01-17 15:50:16 +0530

    content pushed

--- cybersecruity/cyber.js
+++ cybersecruity/cyber.js
@@ -15,3 +15,32 @@ menuBtn.addEventListener("click",()=>{
         menuClick = true;
     }
 })
+
+// code for target div
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Select all anchor tags with the data-target attribute
+    var anchorTags = document.querySelectorAll('.sideContainerRoot a[data-target]');
+
+    // Add click event listener to each anchor tag
+    anchorTags.forEach(function(anchorTag) {
+      anchorTag.addEventListener('click', function(event) {
+        // Prevent default behavior of anchor tag
+        event.preventDefault();
+
+        // Get the target div id from the data-target attribute
+        var targetId = anchorTag.getAttribute('data-target');
+
+        // Find the corresponding div element
+        var targetDiv = document.getElementById(targetId);
+
+        // Apply margin to the target div
+        if (targetDiv) {
+          targetDiv.style.paddingTop = '70px';
+        }
+
+        // Optional: Scroll to the target div
+        targetDiv.scrollIntoView({ behavior: 'smooth' });
+      });
+    });
+  });
